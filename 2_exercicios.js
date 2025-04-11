92c6d6ca9db0aedba961a7220a03d9647bd1d928
Yasmin Porto <yasmin.porto@aluno.senai.br>
2025-04-11 11:24:58 -0300

    2_exercicios.js

---
+++ 2_exercicios.js
@@ -0,0 +1,16 @@
+let numero = 0
+ 
+
+    while(numero <= 15 ){
+        numero ++
+        if (numero % 2 !== 0){
+            console.log(numero)
+        }
+        
+    }
+    //Yasmin Porto
+    
+
+
+
+
